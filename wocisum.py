3b014b3a4bec3cb81ef42d4beb2bdd275e7fc9fa
viiviii <viiiix.viiviii@gmail.com>
2020-12-08 23:24:28 +0900

    add fiddler openSSL settings

--- wocisum.py
+++ wocisum.py
@@ -1,7 +1,5 @@
-from urllib import request
-from bs4 import BeautifulSoup
-import private_constant
 import requests
+import private_constant
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
@@ -9,7 +7,7 @@ headers = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
 }
-res = requests.get(private_constant.target, headers=headers)
+
+proxies = {'http': 'http://127.0.0.1:8888', 'https': 'http:127.0.0.1:8888'}
+res = requests.get(private_constant.target, headers=headers, proxies=proxies, verify=r"FiddlerRoot.pem")
 print(res.text)
-
-
